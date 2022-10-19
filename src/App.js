aceb9487693873ad173c9d00f95851132f64db94
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-10-19 15:31:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,9 @@ import Index from './pages/Index';
 import Item from './pages/Item';
 import Join from './pages/Join';
 import Login from './pages/Login';
-
-
 import Signup3 from './pages/Signup3';
-
-
 import Signup2 from './pages/Signup2';
-
 import Signup1 from './pages/Signup1'
-
 
 function App() {
   return (
